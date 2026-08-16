7353322e115939569ea615128a26208886565aee
Juan Vera <jestebanvera@unicesar.edu.co>
2026-08-16 15:19:36 -0500

    feat: crear clase Persona como padre de Cliente

---
+++ src/main/java/com/mycompany/taller1/biblioteca/git/person.java
@@ -0,0 +1,31 @@
+
+package com.mycompany.taller1.biblioteca.git;
+
+public class person {
+    protected int id;
+    protected String name;
+    protected String phone;
+    public person(int id,String name,String phone){
+        this.id=id;
+        this.name=name;
+        this.phone=phone;
+    }
+    public int getId(){
+        return id;
+    }
+    public String getName(){
+        return name;
+    }
+    public String getPhone(){
+        return phone;
+    }
+    public void setId(int id){
+        this.id=id;
+    }
+    public void setName(String name){
+        this.name=name;
+    }
+    public void setPhone(String phone){
+        this.phone=phone;
+    }
+}
