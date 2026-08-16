58d61a85b5daa1e43ecc72a569c727980954af5e
Juan Vera <jestebanvera@unicesar.edu.co>
2026-08-16 16:34:31 -0500

    fix: corregir nombre de clase Person

--- src/main/java/com/mycompany/taller1/biblioteca/git/person.java
+++ src/main/java/com/mycompany/taller1/biblioteca/git/person.java
@@ -1,11 +1,11 @@
 
 package com.mycompany.taller1.biblioteca.git;
 
-public class person {
+public class Person {
     protected int id;
     protected String name;
     protected String phone;
-    public person(int id,String name,String phone){
+    public Person(int id,String name,String phone){
         this.id=id;
         this.name=name;
         this.phone=phone;
