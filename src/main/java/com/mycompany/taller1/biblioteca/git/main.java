5f9f35147a1cc623c55a40d00854cc5a477b18e8
Juan Vera <jestebanvera@unicesar.edu.co>
2026-08-16 17:02:06 -0500

    feat: implementar CREATE de Cliente

--- src/main/java/com/mycompany/taller1/biblioteca/git/main.java
+++ src/main/java/com/mycompany/taller1/biblioteca/git/main.java
@@ -3,14 +3,34 @@
  */
 
 package com.mycompany.taller1.biblioteca.git;
+import java.util.ArrayList;
+import java.util.Scanner;
 
-/**
- *
- * @author Usuario
- */
 public class main {
-
+    static ArrayList<Client> clients = new ArrayList<>(); 
+    static Scanner sc = new Scanner(System.in);
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        createClient();
+    }
+    //crear cliente
+    public static void createClient(){
+        System.out.print("enter the customer ID: ");
+        int id = sc.nextInt();
+        sc.nextLine();
+
+        System.out.print("enter the customer's name: ");
+        String nombre = sc.nextLine();
+
+        System.out.print("Enter the customer's phone number: ");
+        String telefono = sc.nextLine();
+
+        System.out.print("Enter the customer's email address: ");
+        String email = sc.nextLine();
+
+        Client client = new Client(id, nombre, telefono, email);
+
+        clients.add(client);
+
+        System.out.println("Client created successfully.");
     }
 }
